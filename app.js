ff3557cec5d9ca90c76b7a5320a1435df4fb3ea5
Soumyadip Das <soumyadipdas404@gmail.com>
2024-10-22 22:46:37 +0530

    this

--- app.js
+++ app.js
@@ -4,38 +4,23 @@ const express = require('express');
 const config = require('./config/config');
 const { default: mongoose } = require('mongoose');
 const logRoute = require('./middleware/logger');
-const http = require('http');
-const path = require('path');
-const { initializeWebSocket, handleUpgrade } = require('./controller/webScocket.controller');
 
 // route init
 const userRoutes = require('./routes/user.route');
 const indexRouter = require('./routes/index.route');
 const itemRoutes = require('./routes/item.route');
 const categoryRoutes = require('./routes/category.route');
-const websocketRoutes = require('./routes/webScocket.route');
-
 
 // App init
 const app = express();
-const server = http.createServer(app);
-// Initialize WebSocket server
-initializeWebSocket(server);
-// Serve static files (like the mobile QR scanner page)
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cors());
 app.use(logRoute)
 
 app.use("/api/user", userRoutes.userRoutes);
 app.use("/", indexRouter.indexRouter);
-app.use('/socket', websocketRoutes);
 app.use("/api/item", itemRoutes.itemRoutes);
 app.use("/api/category", categoryRoutes.categoryRoutes);
-
-server.on('upgrade', (request, socket, head) => {
-    handleUpgrade(request, socket, head);
-});
 
 // Db connect
 mongoose.connect(config.dburi)
