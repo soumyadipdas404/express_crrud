3062c5252088cdd13c96aaebc5abda6d7681cc61
Soumyadip Das <soumyadipdas404@gmail.com>
2024-11-15 00:42:29 +0530

    this

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const categoryRoutes = require('./routes/category.route');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(logRoute); //test hiiii
+app.use(logRoute); //test
 
 app.use("/api/user", userRoutes.userRoutes);
 app.use("/", indexRouter.indexRouter);
